87acdc849a5b82c73639fca764c1da7de0ff13da
JinfeiChen <jinfei_chen@icloud.com>
2017-08-29 13:45:54 +0800

    add test folder

---
+++ test/CJFTest.h
@@ -0,0 +1,13 @@
+//
+//  CJFTest.h
+//  Pods
+//
+//  Created by cjf on 2017/8/29.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CJFTest : NSObject
+
+@end
---
+++ test/CJFTest.m
@@ -0,0 +1,13 @@
+//
+//  CJFTest.m
+//  Pods
+//
+//  Created by cjf on 2017/8/29.
+//
+//
+
+#import "CJFTest.h"
+
+@implementation CJFTest
+
+@end
